e2f838769e66cdf4428ec4cc64014801b7740b4f
Daan <daan@zimmermanzimmerman.nl>
2015-09-01 11:59:39 +0200

    nu met class active op menu

--- narrowcasting_server/static/javascripts/layout/controllers/navbar.controller.js
+++ narrowcasting_server/static/javascripts/layout/controllers/navbar.controller.js
@@ -9,17 +9,27 @@
 		.module('ncs.layout.controllers')
 		.controller('NavbarController', NavbarController);
 
-	NavbarController.$inject = ['$scope', 'Authentication'];
+	NavbarController.$inject = ['$scope', '$state', 'Authentication'];
 
 	/** 
 	* @namespace NavBarController
 	*/
-	function NavbarController($scope, Authentication){
+	function NavbarController($scope, $state, Authentication){
 		var vm = this;
 
 		vm.isAuthenticated = Authentication.isAuthenticated();
 
 		vm.logout = logout;
+
+		vm.stateName = '';
+	    vm.state = $state;
+
+		$scope.$watch('vm.state.current.name', function(name){
+	        if (['displays','new-display'].indexOf(name) >= 0) { vm.stateName = 'displays'; }
+	        else if (['presentations','edit-presentation'].indexOf(name) >= 0) { vm.stateName = 'presentations'; }
+	        else if (['learn'].indexOf(name) >= 0) { vm.stateName = 'learn'; }
+	        else { vm.stateName = 'home'; }
+	    }, true);
 
 		/**
 	    * @name logout
